f0d0f1b2667c1bef1b768b601d53f2cc3123adb6
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2022-11-28 18:29:42 +0600

    Handle loader & error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,32 @@
 import React from "react";
-import ProductCard from '../components/ProductCard';
+import ProductCard from "../components/ProductCard";
 import { useProducts } from "../context/ProductProvider";
 
 const Home = () => {
+  const {
+    state: { products, loading, error },
+  } = useProducts();
 
-  const {state:{products}} = useProducts();
+  let content;
+
+  if (loading) {
+    content = <p>Loading...</p>;
+  }
+  if (!loading && !error && products.length === 0) {
+    content = <p>Product list not found</p>;
+  }
+  if (error) {
+    content = <p>Something went wrong</p>;
+  }
+  if (!loading && !error && products.length) {
+    content = products.map((product) => (
+      <ProductCard key={product._id} product={product} />
+    ));
+  }
 
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      {
-        products.map((product)=> <ProductCard product={product}/>)
-      }
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
+      {content}
     </div>
   );
 };
